1be5a835f889bd435e04346ddea562f1b2ae7509
Gabriel <gabrielrubama@hotmail.com>
2023-03-22 22:49:36 -0600

    commit final

---
+++ src/data/tasks.js
@@ -0,0 +1,19 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "mi primer tarea",
+    description: "Mi primer tarea",
+  },
+
+  {
+    id: 1,
+    title: "mi primer tarea 2",
+    description: "Mi primer tarea 2",
+  },
+
+  {
+    id: 2,
+    title: "mi primer tarea 3",
+    description: "Mi primer tarea 3",
+  },
+];
